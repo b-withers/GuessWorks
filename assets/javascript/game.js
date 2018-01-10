90ba65c17bc3f08254f7a4cbce45b1d62cbfcdb5
Brady <brady.withers@outlook.com>
2018-01-09 20:29:30 -0700

    word change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var words = ["DarthVader"];
+var words = ["fox"];
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var count = 10;
 var answer = "";
