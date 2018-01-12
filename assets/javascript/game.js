b4b7f7d8dc18a66aeee01c3709ee7e790ca881e9
Brady <brady.withers@outlook.com>
2018-01-12 14:40:12 -0700

    more gifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var words = ["fox", "racoon", "bear", "elephant", "puppy", "rabbit", "deer", "whale", "dolphin", "chicken", "monkey", "beaver", "horse", "kitten", "buffalo", "cheetah", "flamingo", "panda", "hamster", "polarbear", "pig", "giraffe", "alligator", "moose", "chimpanzee", "goose", "donkey", "mouse", "jellyfish", "kangaroo", "owl", "penguin", "rhinoceros", "sheep", "skunk", "sloth", "bunny", "turkey", "zebra", "yak", "Caterpillar","crocodile", "dove", "eagle", "falcon", "frog", "gorila", "Hedgehog", "Hummingbird", "Koala", "Leopard", "seal", "Otter", "porcupine", "ram", "rat", "squirrel", "gazelle"];
+var words = ["fox", "racoon", "bear", "elephant", "puppy", "rabbit", "deer", "whale", "dolphin", "chicken", "monkey", "beaver", "horse", "kitten", "buffalo", "cheetah", "flamingo", "panda", "hamster", "polarbear", "pig", "giraffe", "alligator", "moose", "chimpanzee", "goose", "donkey", "mouse", "jellyfish", "kangaroo", "owl", "penguin", "rhinoceros", "sheep", "skunk", "sloth", "bunny", "turkey", "zebra", "yak", "Caterpillar","crocodile", "dove", "eagle", "falcon", "frog", "gorila", "Hedgehog", "Hummingbird", "Koala", "Leopard", "seal", "Otter", "porcupine", "ram", "rat", "squirrel", "gazelle", "antelope", "camel", "Hippopotamus", "llama"];
 
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -9,11 +9,17 @@ var answer = "";
 var audioElement = document.createElement("audio");
       audioElement.setAttribute("src", "assets/push.mp3");
 
+      // var winnerElement = document.createElement("audio");
+      // winnerElement.setAttribute("src", "assets/winner.mp3");
+
+      // var LoserElement = document.createElement("audio");
+      // LoserElement.setAttribute("src", "assets/laugh.mp3");
+
 // var wins = " ";
 // var losses =" ";
 
 
-var random_images_array = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif", "8.gif", "9.gif", "10.gif", "11.gif", "12.gif", "13.gif", "14.gif", "15.gif", "16.gif", "17.gif", "18.gif", "19.gif", "20.gif", "21.gif", "22.gif", "23.gif", "24.gif", "25.gif", "26.gif", "27.gif", "28.gif", "29.gif", "30.gif", "32.gif", "33.gif"];
+var random_images_array = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif", "8.gif", "9.gif", "10.gif", "11.gif", "12.gif", "13.gif", "14.gif", "15.gif", "16.gif", "17.gif", "18.gif", "19.gif", "20.gif", "21.gif", "22.gif", "23.gif", "24.gif", "25.gif", "26.gif", "27.gif", "28.gif", "29.gif", "30.gif", "32.gif", "33.gif", "34.gif", "35.gif", "36.gif", "37.gif", "38.gif", "39.gif", "40.gif", "41.gif", "42.gif", "43.gif", "44.gif", "45.gif"];
 
 function getRandomImage(imgAr, path) {
   path = path || 'assets/images/'; 
@@ -60,6 +66,7 @@ function createLetterButtons() {
           
         if($("#blank-word").children().text().indexOf("_ ") == -1){
           //wins ++;
+          //winnerElement.play();
           $("#buttons").remove();
           $("#title").text("That is Correct");
           $("#play").append("<br><button onclick='reloadPage()'>Play Again?</button>");
@@ -73,6 +80,7 @@ function createLetterButtons() {
           $("#count").text(count);
           
 	        if(count==0){
+            //LoserElement.play();
             //losses ++;
             $("#blank-word").replaceWith('<div class="answer">' + answer + '</div');
 	        	$("#buttons").remove();
