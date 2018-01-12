c6d050a7a14111f34fda45e2ed849f67827de24c
Brady <brady.withers@outlook.com>
2018-01-11 17:59:13 -0700

    gifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ var answer = "";
 // var losses =" ";
 
 
-var random_images_array = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif", "8.gif", "9.gif", "10.gif", "11.gif", "12.gif", "13.gif", "14.gif", "15.gif", "16.gif"];
+var random_images_array = ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif", "8.gif", "9.gif", "10.gif", "11.gif", "12.gif", "13.gif", "14.gif", "15.gif", "16.gif", "17.gif", "18.gif", "19.gif", "20.gif", "21.gif", "22.gif", "23.gif"];
 
 function getRandomImage(imgAr, path) {
   path = path || 'assets/images/'; 
